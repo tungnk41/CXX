04269e74ba1dd5e64bd4314efdf45416f91c0146
tungnk41 <tung.nk41@gmail.com>
2023-05-01 20:00:25 +0700

    add boost example

---
+++ project/Cmake/boost/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <boost/math/special_functions/factorials.hpp>
+
+int main()
+{
+    int num = 2;
+    std::cout << "Factorial of " << num << " is " << boost::math::factorial<double>(num) << std::endl;
+
+    system("pause");
+    return 0;
+}
