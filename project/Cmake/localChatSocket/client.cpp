605611137b3ff3b4e9b9f117dc83a7860eff8710
tungnk41 <tungnk41>
2023-05-22 17:47:12 +0700

    update

--- project/Cmake/localChatSocket/client.cpp
+++ project/Cmake/localChatSocket/client.cpp
@@ -7,8 +7,14 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <iostream>
+
+#include <sys/un.h>
 #define MAX 255
 #define PORT 5050
+
+# define CLIENT_SOCK_FILE "client.sock"
+# define SERVER_SOCK_FILE "server.sock"
+
 
 using namespace std;
  
@@ -19,8 +25,10 @@ int main(int argc, char const *argv[]) {
     int port_no = PORT;
     
     struct sockaddr_in servaddr;
+    struct sockaddr_un server_address, client_address;
  
-    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);        
+    // int sockfd = socket(AF_INET, SOCK_DGRAM, 0);        
+    int sockfd = socket(AF_UNIX, SOCK_DGRAM, 0);        
 
     if(sockfd < 0){                                    //returns non negative integer if successful
         perror("Failed to create a socket");
@@ -32,27 +40,39 @@ int main(int argc, char const *argv[]) {
     //The port that receives the data is important so we bind the socket in
     // the server program
 
-    memset(&servaddr, 0, sizeof(servaddr));
- 
-    // Filling server information
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = INADDR_ANY;
-    servaddr.sin_port = htons(port_no);
+    // memset(&servaddr, 0, sizeof(servaddr));
+    // // Filling server information
+    // servaddr.sin_family = AF_INET;
+    // servaddr.sin_addr.s_addr = INADDR_ANY;
+    // servaddr.sin_port = htons(port_no);
+
+        // Name the socket then bind
+    memset(&server_address, 0, sizeof(server_address));
+    memset(&client_address, 0, sizeof(client_address));
+    client_address.sun_family = AF_UNIX;
+    strcpy(client_address.sun_path, CLIENT_SOCK_FILE);
+    unlink(CLIENT_SOCK_FILE);
+    bind(sockfd, (struct sockaddr *)&client_address, sizeof(client_address));
+
+    // Name the socket
+    server_address.sun_family = AF_UNIX;
+    strcpy(server_address.sun_path, SERVER_SOCK_FILE);
+
      
     int n;
-    socklen_t len = sizeof(servaddr);
+    socklen_t len = sizeof(server_address);
 
     while(1){
 
         fgets(str, MAX, stdin);
      
         sendto(sockfd, (const char *)str, strlen(str),
-            0, (const struct sockaddr *) &servaddr, 
-            sizeof(servaddr));
+            0, (const struct sockaddr *) &server_address, 
+            sizeof(server_address));
     
          
         n = recvfrom(sockfd, (char *)buffer, MAX, 
-                0, (struct sockaddr *) &servaddr,
+                0, (struct sockaddr *) &server_address,
                 &len);
 
         buffer[n] ='\0';
