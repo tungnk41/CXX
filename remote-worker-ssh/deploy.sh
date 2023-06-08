4351e8da5bfb9576bca401003de2475087048614
tung6.nguyen <tung6.nguyen@lge.com>
2023-06-08 15:19:41 +0700

    update

--- remote-worker-ssh/deploy.sh
+++ remote-worker-ssh/deploy.sh
@@ -8,11 +8,17 @@ modules=(
 
 host="worker@tung6-nguyen-24dcm-tn.vbee.lge.com"
 port="42113"
+adb="_adb"
 
 DOWNLOAD_IPK(){
 	model="sa515m"
 	version_module="1.0-r0"
 	deploy_target="armv7at2hf-neon"
+
+	rm -rf tmp
+	mkdir tmp
+	cd tmp
+
 	for pair in "${modules[@]}"; do
     		KEY="${pair%%:*}"
     		VALUE="${pair##*:}"
@@ -20,21 +26,17 @@ DOWNLOAD_IPK(){
 		ipk_file=$VALUE
 		rm -rf $ipk_file
 		echo 
-		echo "Downloading ...... "
 		path="~/build-24dcm/nad/$model/${model^^}_apps/apps_proc/build/tmp-glibc/work/armv7at2hf-neon-oe-linux-gnueabi/$module/$version_module/deploy-ipks/$deploy_target/$ipk_file"
 		echo $path
 		scp -P $port $host:$path . 
 		echo
 	done
+	cd ..
 }
 
 PUSH_IPK(){
-	
-	adb="_adb"
-	cmd
-	$adb root
-	$adb shell mount -o remount,rw /
-	bash
+	./root.bat
+	cd tmp
 	for pair in "${modules[@]}"; do
     		KEY="${pair%%:*}"
     		VALUE="${pair##*:}"
@@ -42,15 +44,59 @@ PUSH_IPK(){
 		ipk_file=$VALUE
 		echo 
 		echo "Uploading ...... "
-		cmd
 		$adb push $ipk_file tmp/
 		$adb shell opkg install --force-reinstall --force-overwrite tmp/$ipk_file
 		$adb shell rm -rf $ipk_file
-		$adb shell sync
-		bash
+		$adb shell sync	
 		echo
-	done	
+	done
+	cd ..	
 }
 
-#DOWNLOAD_IPK
+FORWARD_LOG(){
+	echo "Forward Port: 3490 =>>> DLT LOG"
+	adb forward tcp:3490 tcp:3490
+}
+
+ALL(){
+FORWARD_LOG
+DOWNLOAD_IPK
 PUSH_IPK
+}
+
+Exit(){
+	echo "Exit"
+	exit
+}
+
+#Loop 
+while :
+do
+	echo "1. All"
+	echo "2. Download IPK"
+	echo "3. Push IPK"
+	echo "4. Forward Log"
+	echo "5. Exit"
+	read -p "Option : " option
+
+	case $option in
+		2)
+		DOWNLOAD_IPK
+		;;
+		3)
+		PUSH_IPK
+		;;
+		4)
+		FORWARD_LOG
+		;;
+		1)
+		ALL
+		;;
+		5)
+		Exit
+		;;
+		*)	
+		echo "*******"
+		;;
+	esac
+done
---
+++ remote-worker-ssh/root.bat
@@ -0,0 +1,2 @@
+_adb root
+_adb shell mount -o remount,rw /
